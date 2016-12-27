389c426c8d2b27f106895b6736f9a0f1cd9fbab9
Ravi Mathpal <ravimail26@gmail.com>
2016-12-27 22:18:57 +0530

    Fetched text into textview

--- app/src/main/java/com/example/rishabh/kendoeditordemo/MainActivity.java
+++ app/src/main/java/com/example/rishabh/kendoeditordemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rishabh.kendoeditordemo;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -24,16 +26,27 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.setInitialScale(1);
-        webView.addJavascriptInterface(new MyJavaScriptInterface(textView), "INTERFACE");
         webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
+        webView.addJavascriptInterface(new MyJavaScriptInterface(textView), "INTERFACE");
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageFinished(WebView view, String url)
-            {
+            public void onPageFinished(WebView view, String url) {
                 view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementById('editor').value);");
             }
         });
         webView.loadUrl("file:///android_asset/test.html");
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementById('editor').value);");
+                    }
+                });
+            }
+        });
 
 
     }
@@ -46,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @SuppressWarnings("unused")
-
+        @JavascriptInterface
         public void processContent(String aContent) {
             final String content = aContent;
             contentView.post(new Runnable() {
