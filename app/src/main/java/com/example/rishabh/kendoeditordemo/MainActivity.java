b4070d8ee56a0c7df85900fb906346783e2bf6e9
Ravi Mathpal <ravimail26@gmail.com>
2016-12-27 22:07:35 +0530

    Trying to load text to textview

--- app/src/main/java/com/example/rishabh/kendoeditordemo/MainActivity.java
+++ app/src/main/java/com/example/rishabh/kendoeditordemo/MainActivity.java
@@ -1,27 +1,61 @@
 package com.example.rishabh.kendoeditordemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView mwebView ;
+    WebView webView;
+    TextView textView;
 
-
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mwebView = (WebView) findViewById(R.id.htmlView);
+        textView = (TextView) findViewById(R.id.textView);
+        webView = (WebView) findViewById(R.id.htmlView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setInitialScale(1);
+        webView.addJavascriptInterface(new MyJavaScriptInterface(textView), "INTERFACE");
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementById('editor').value);");
+            }
+        });
+        webView.loadUrl("file:///android_asset/test.html");
 
-        mwebView.loadUrl("file:///android_asset/mvvm.html");
 
-
-     // mwebView.loadData(summary,"text/html",null);
     }
+
+    class MyJavaScriptInterface {
+        private TextView contentView;
+
+        public MyJavaScriptInterface(TextView aContentView) {
+            contentView = aContentView;
+        }
+
+        @SuppressWarnings("unused")
+
+        public void processContent(String aContent) {
+            final String content = aContent;
+            contentView.post(new Runnable() {
+                public void run() {
+                    contentView.setText(content);
+                }
+            });
+        }
+    }
+
+
 }
